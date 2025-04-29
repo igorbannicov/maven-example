0a4f1ff3db7b714e09a2f292b63fcc6e9a640096
Igor Bannicov <igor.bannicov@endava.com>
2025-04-29 13:28:56 +0300

    Update code

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from EC2!");
+        System.out.println("\e[32Hello from EC2!\e[0m");
     }
 }
