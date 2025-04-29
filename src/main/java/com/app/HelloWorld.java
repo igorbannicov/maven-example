a95c300a073c97f0ddc1d2f32e7c53371bc1f276
Igor Bannicov <igor.bannicov@endava.com>
2025-04-29 13:22:17 +0300

    Update code

--- src/main/java/com/app/HelloWorld.java
+++ src/main/java/com/app/HelloWorld.java
@@ -1,7 +1,7 @@
-package com.app;
+package com.example;
 
-public class HelloWorld {
-    public static void main(String[] argv) {
-        System.out.println("Hello world");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from EC2!");
     }
 }
